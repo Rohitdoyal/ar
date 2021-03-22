8d174d7ed0088b539321c7da34516bf0bb39b1f6
ROHIT DOYAL <56929395+Rohitdoyal@users.noreply.github.com>
2021-03-23 00:12:01 +0530

    gamemanager and main menu file
    gamemanager which is control whole project and main menu help us to goes any scene whatever we want

---
+++ end.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class end : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float timeRemaining = 10;
+
+    void Update()
+    {
+        if (timeRemaining > 0)
+        {
+            timeRemaining -= Time.deltaTime;
+        }
+
+        else{
+           Debug.Log("quit");
+           Application.Quit();
+        }
+    }
+
+}
---
+++ mainmenu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainmenu : MonoBehaviour
+{   
+
+    public int levelname;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerdata data= savesystem.Loadplayer();
+        if (data==null)
+        {
+            levelname=1;
+            Debug.Log(" level start hua inside start nulladata set  "+levelname);
+        }
+        else{
+        levelname =  data.levelname;
+        Debug.Log(" level start hua inside start  "+levelname);
+        }
+        if (levelname==0)
+        {
+            levelname=levelname+1;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void play(){
+       // levelname=gm.levelname;
+        SceneManager.LoadScene(levelname);
+        Debug.Log(" level start hua "+levelname);
+    }
+}
